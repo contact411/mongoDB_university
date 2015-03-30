b849b46490d6e4d1bba9a9d21fca1299701df055
Joel Hampton <joelhampton@gmail.com>
2015-03-30 22:44:34 +1100

    Completed Node.js Driver inserting id

--- week_2_crud/node_js_driver_inserting_id/insert_id/insert_id/app.js
+++ week_2_crud/node_js_driver_inserting_id/insert_id/insert_id/app.js
@@ -3,7 +3,7 @@ var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
     if(err) throw err;
 
-    var doc = { '_id' : 'calvin', 'age' : 6 };
+    var doc = { '_id' : 'Joel', 'age' : 38 };
 
     db.collection('students').insert(doc, function(err, inserted) {
         if(err) throw err;
